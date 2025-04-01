814aad4992ca934dc5c5cc3c8adca74f060a96c8
Wahyudi <wap.youdi@gmail.com>
2025-04-01 10:40:48 +0700

    Create post.js

---
+++ pagination/post.js
@@ -0,0 +1,15 @@
+// post.js
+
+// Sample articles data
+const articles = [
+  { title: 'How to Create Your Own Soap Recipes', description: 'Discover the secrets of crafting unique soap recipes...', productUrl: 'https://ratakan.com/product/resep-sabun-lengkap-955' },
+  { title: 'Mastering Digital Marketing', description: 'Learn how to grow your business online with proven strategies...', productUrl: 'https://ratakan.com/product/digital-marketing-101' },
+  { title: 'The Art of Photography', description: 'Explore techniques to capture stunning photos...', productUrl: 'https://ratakan.com/product/photography-course' },
+  { title: 'Cooking Like a Pro', description: 'Step-by-step guide to becoming a master chef...', productUrl: 'https://ratakan.com/product/cooking-class' },
+  { title: 'Gardening for Beginners', description: 'Start your own garden with these easy tips...', productUrl: 'https://ratakan.com/product/gardening-guide' },
+  { title: 'Advanced Yoga Techniques', description: 'Deepen your practice with advanced yoga poses...', productUrl: 'https://ratakan.com/product/yoga-course' },
+  { title: 'Writing Your First Novel', description: 'Turn your ideas into a bestselling book...', productUrl: 'https://ratakan.com/product/writing-course' },
+];
+
+// Export articles for use in script.js
+export default articles;
